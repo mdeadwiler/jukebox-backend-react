89cd57f3bb97713bd724d41f837a5d1cb03a8daf
MDeadwiler <mdeadwiler@email.com>
2024-10-20 14:23:46 -0400

    whatever

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ import dotenv from "dotenv"
 dotenv.config();
 import trackRouter from "./routes/tracks.js"
 import cors from "cors";
+
 const app = express()
 const PORT = process.env.PORT || 3000
+
+
+
 app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
